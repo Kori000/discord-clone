eee0fef735ff708636185bc30ca4dd89ba4d6b6c
Kori <kexin@korix.top>
2024-02-20 12:39:20 +0800

    feat: initial channel

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -23,9 +23,13 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const icon = roleIconMap[member.role]
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+  }
+
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       type='button'
       className={cn(
         'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
