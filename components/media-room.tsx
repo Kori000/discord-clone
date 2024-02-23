c580cdca9eeda3ac979976e83d8e6db3f160192d
Kori <kexin@korix.top>
2024-02-23 16:00:36 +0800

    feat: audio room pending

---
+++ components/media-room.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { LiveKitRoom, VideoConference } from '@livekit/components-react'
+import '@livekit/components-styles/'
+import { Channel } from '@prisma/client'
+import { useUser } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+
+interface MediaRoomProps {
+  chatId: string
+  video: boolean
+  audio: boolean
+}
+
+export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
+  const { user } = useUser()
+
+  return <div></div>
+}
